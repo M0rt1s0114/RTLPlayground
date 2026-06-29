28ff7be4797f097b557a86ee26f68060bbbd996f
Mortis0114 <46196322+M0rt1s0114@users.noreply.github.com>
2026-06-30 05:49:55 +0800

    Make a modern UI

--- html/navigation.js
+++ html/navigation.js
@@ -1,12 +1,29 @@
-document.getElementById('sidebar').innerHTML =
- "<ul><li><a href='index.html'>Overview</a></li>"
- + "<li><a href='ports.html'>Port Configuration</a></li>"
- + "<li><a href='stat.html'>Port Statistics</a></li>"
- + "<li><a href='vlan.html'>VLAN</a></li>"
- + "<li><a href='l2.html'>L2 Configuration</a></li>"
- + "<li><a href='mirror.html'>Mirroring</a></li>"
- + "<li><a href='lag.html'>Link Aggregation</a></li>"
- + "<li><a href='eee.html'>EEE</a></li>"
- + "<li><a href='bandwidth.html'>Bandwidth Limits</a></li>"
- + "<li><a href='system.html'>System Settings</a></li>"
- + "<li><a href='update.html'>Firmware Update</a></li></ul>";
+(function() {
+  const path = location.pathname.split('/').pop() || 'index.html';
+  const links = [
+    { href: 'index.html',     label: 'Overview',         icon: '⬡' },
+    { href: 'ports.html',     label: 'Port Config',      icon: '⛭' },
+    { href: 'stat.html',      label: 'Port Statistics',  icon: '◈' },
+    { href: 'vlan.html',      label: 'VLAN',             icon: '◧' },
+    { href: 'l2.html',        label: 'L2 Table',         icon: '⊞' },
+    { href: 'mirror.html',    label: 'Mirroring',        icon: '⇆' },
+    { href: 'lag.html',       label: 'Link Aggregation', icon: '⇌' },
+    { href: 'eee.html',       label: 'EEE',              icon: '⚡' },
+    { href: 'bandwidth.html', label: 'Bandwidth Limits', icon: '◉' },
+    { href: 'system.html',    label: 'System Settings',  icon: '⚙' },
+    { href: 'update.html',    label: 'Firmware Update',  icon: '↑' },
+  ];
+  const ul = document.createElement('ul');
+  links.forEach(function(l) {
+    const li = document.createElement('li');
+    const a  = document.createElement('a');
+    a.href = l.href;
+    a.setAttribute('data-icon', l.icon);
+    a.textContent = l.label;
+    if (l.href === path) a.classList.add('active');
+    li.appendChild(a);
+    ul.appendChild(li);
+  });
+  const nav = document.getElementById('sidebar');
+  if (nav) nav.appendChild(ul);
+})();
